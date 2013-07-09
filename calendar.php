d6b56c68b1bfac6af1cd52170f33ee423c285e46
Felix Kronlage <kronlage@bytemine.net>
2013-07-09 10:34:58 +0200

    sprinkle bsd license header

--- calendar.php
+++ calendar.php
@@ -1,5 +1,20 @@
 <?php
-    // Copyright (c) 2013 bytemine GmbH
+	/*
+	 * Copyright (c) 2009, 2010 bytemine GmbH <info@bytemine.net>
+	 *
+	 * Permission to use, copy, modify, and distribute this software for any
+	 * purpose with or without fee is hereby granted, provided that the above
+	 * copyright notice and this permission notice appear in all copies.
+	 *
+	 * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+	 * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+	 * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+	 * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+	 * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+	 * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+	 * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+	 */
+
     // Author: Daniel Rauer <rauer@bytemine.net>
     // http://www.bytemine.net/
     
