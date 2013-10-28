ddbf6fd96732a62438b1b6ae1f0100c835aef587
Daniel Rauer <rauer@bytemine.net>
2013-10-29 00:14:29 +0100

    Adapt copyright date to 2013.

--- calendar.php
+++ calendar.php
@@ -1,6 +1,6 @@
 <?php
 	/*
-	 * Copyright (c) 2009, 2010 bytemine GmbH <info@bytemine.net>
+	 * Copyright (c) 2013 bytemine GmbH <info@bytemine.net>
 	 *
 	 * Permission to use, copy, modify, and distribute this software for any
 	 * purpose with or without fee is hereby granted, provided that the above
